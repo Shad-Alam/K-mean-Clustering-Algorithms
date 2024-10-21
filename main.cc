69ae06c579d9fad9ade61f14d673527b8e9b9177
Shad-Alam <shad.19bd@gmail.com>
2024-10-21 23:42:51 +0600

    calculate distance between two point using euclidean distance method and finding closer to original point

--- main.cc
+++ main.cc
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 
-//using namespace std;
-
 int main(){
 	freopen("DATA_SET.txt", "r", stdin);
 	
 	char ch; int n1, n2;
-	std::vector<std::pair<char,std::pair<int,int>>> dataSet;
+
+	std::vector<std::pair<char,std::pair<double,double>>> dataSet;
 	
 	while(std::cin >> ch >> n1 >> n2){
 		if(ch=='-1' && n1==n2 && n1==-1){
@@ -22,7 +21,7 @@ int main(){
 	int centroids = k, n = dataSet.size();
 	
 	// Step-3: choose randomly from dataset for centroid and store it
-	std::vector<std::pair<char,std::pair<int,int>>> centroid;
+	std::vector<std::pair<char,std::pair<double,double>>> centroid;
 	// for example i will choose  first k index for testing
 	
 	// if(k>n) give a error 
@@ -30,9 +29,36 @@ int main(){
 		centroid.push_back({dataSet[a].first,{dataSet[a].second.first, dataSet[a].second.second}});
 	}
 	
-	for(int a=0; a<n; a++){
-		// point a
+
+	int iteration = 1;
+	
+	std::map<int, std::vector<std::pair<char,std::pair<int,int>>>> i;
+	while(true){
+		for(int a=0; a<n; a++){
+			// point a:(1,1)
+			// cen-1:(1,1)
+			// 		dis = 0
+			// cen-2:(2,1)
+			//		dis = sqrt((x2-x1)^2+(y2-y1)^2)
+			double x1 = dataSet[a].second.first, y1 = dataSet[a].second.second;
+			std::vector<std::pair<double,int>> iv;
+			for(int b=0; b<k; b++){
+				double x2 = dataSet[a].second.first, y2 = dataSet[a].second.second;
+				// Euclidean distance between two point
+				double distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+				
+				iv.push_back({distance,b});
+			}
+			
+			sort(iv.begin(), iv.end());
+			// assign to centroid (closer to point a)
+			i[iv[0].second].push_back(dataSet[a]);
+		}
+		
+		iteration++;
+		break;
 	}
+	
 	
 	return 0;
 }
